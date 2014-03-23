28185a229575f7cff2129acf6fcc6bb827eb1051
Steven Frink <sfrink1@gmail.com>
2014-03-23 14:19:20 -0400

    added main

--- src/server/Main.java
+++ src/server/Main.java
@@ -1,7 +1,12 @@
 package server;
 
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.util.Iterator;
 
+import javax.crypto.Cipher;
+
+import rmi.CertusServer;
 import database.DatabaseConnector;
 import dto.*;
 import enumeration.*;
@@ -36,6 +41,32 @@ public class Main {
 		for (CandidateDto c : db.selectCandidatesOfElection(1)) {
 			System.out.println(c.toString());
 		}*/
+		try {
+			CertusServer serv=new CertusServer();
+			PublicKey pk=(PublicKey)serv.getTallierPublicKey().getObject();
+			System.out.println(pk.toString());
+			Cipher enc = Cipher.getInstance("RSA");
+			  // Note 1:  "ECB" in line above is bogus.  Doesn't actually encrypt
+			  //   more than one block.
+			  // Note 2:  Encoding function above is OAEP.
+			enc.init(Cipher.ENCRYPT_MODE, pk);
+			String m1 = "Attack at dawn"; 
+			byte[] b1 = m1.getBytes();   
+			byte[] c = enc.doFinal(b1);
+			SecurityValidator sec=new SecurityValidator();
+			PrivateKey sk=sec.getPrivateKey();
+			System.out.println(sk.toString());
+			String hex=sec.byteArraytoHex(c);
+			String pt=sec.decrypt(hex);
+			byte[] plain=sec.hexStringtoByteArray(pt);
+			String message=new String(plain);
+			System.out.println(message);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
