109cab8c93f50b1c31ca9545d2daf227f1d1aae0
Hirosh <sulochane@yahoo.com>
2014-03-23 17:17:28 -0400

    fixed reference errors

--- src/server/SecurityValidator.java
+++ src/server/SecurityValidator.java
@@ -34,6 +34,10 @@ public class SecurityValidator {
 		securityKeystoreFile = securityKeyBasePath + ConfigurationProperties.securityKeystoreTallierFile();
 		securityKeystorePrivatekey = securityKeyBasePath + ConfigurationProperties.securityKeystoreTallierPrivatekey();
 		
+		System.out.println(securityKeystoreFile);
+		System.out.println(securityKeystorePrivatekey);
+		System.out.println(securityKeystorePassword);
+		System.out.println(securityKeystoreAlias);
 	}
 	public Validator checkSignature(String sig, int user_id) {
 		DatabaseConnector dbc = new DatabaseConnector();
