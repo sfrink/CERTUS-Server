652dd4a2ca71bf12c1e06e925e24528a1e339ae0
Hirosh <sulochane@yahoo.com>
2014-03-23 17:03:29 -0400

    read sec params from config

--- src/server/SecurityValidator.java
+++ src/server/SecurityValidator.java
@@ -19,7 +19,22 @@ import database.DatabaseConnector;
 import dto.Validator;
 
 public class SecurityValidator {
-
+	
+	private static String securityKeyBasePath;
+	private static String securityKeystoreAlias;
+	private static String securityKeystorePassword;
+	private static String securityKeystoreFile;
+	private static String securityKeystorePrivatekey;
+	
+	public SecurityValidator()
+	{
+		securityKeyBasePath = ConfigurationProperties.securityKeyBasePath();
+		securityKeystoreAlias = ConfigurationProperties.securityKeystoreTallierAllias();
+		securityKeystorePassword = ConfigurationProperties.securityKeystoreTallierPassword();
+		securityKeystoreFile = securityKeyBasePath + ConfigurationProperties.securityKeystoreTallierFile();
+		securityKeystorePrivatekey = securityKeyBasePath + ConfigurationProperties.securityKeystoreTallierPrivatekey();
+		
+	}
 	public Validator checkSignature(String sig, int user_id) {
 		DatabaseConnector dbc = new DatabaseConnector();
 		String pk = (String) dbc.getPubKeyByUserID(user_id).getObject();
