62b6a09f92165ffb99b7b42fddb20fa13ce8f8f8
Hirosh <sulochane@yahoo.com>
2014-03-27 02:49:30 -0400

    fixed the hexStringtoByteArray method array overflow

--- src/server/SecurityValidator.java
+++ src/server/SecurityValidator.java
@@ -79,10 +79,13 @@ public class SecurityValidator {
 
 	public byte[] hexStringtoByteArray(String hex) {
 		int len = hex.length();
-		byte[] data = new byte[len / 2];
-		for (int i = 0; i < len; i += 2) {
-			data[i / 2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4) + Character
-					.digit(hex.charAt(i + 1), 16));
+		byte[] data = new byte[0];
+		if (len % 2 == 0) {
+			data = new byte[len / 2];
+			for (int i = 0; i < len; i += 2) {
+				data[i / 2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4) + Character
+						.digit(hex.charAt(i + 1), 16));
+			}
 		}
 		return data;
 	}
